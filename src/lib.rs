6925fa88fea1923c1119260160a723b793654514
SkyezerFox <actuallyori@gmail.com>
2022-02-06 21:36:04 +0000

    chore: use explicit named modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
-use tonic::include_proto;
-
-include_proto!("dysnomia");
+pub mod services {
+	//! Provides Rust bindings for the gRPC "services" package.
+	tonic::include_proto!("dysnomia.services");
+}
