c9e1e7ec0c0fef7c86868b38d5814343cb201301
SkyezerFox <actuallyori@gmail.com>
2022-02-06 21:06:31 +0000

    feat: create base RPC implementation for service lookup

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,3 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
+use tonic::include_proto;
+
+include_proto!("dysnomia");
